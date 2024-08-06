2a70e37aceb84ff90cae73a186663950617f97e9
Adriel Ribeiro <adrielsousaribeiro69@gmail.com>
2024-08-06 00:24:44 -0300

    realizado mais algumas models

--- Jalider-main/App/models.py
+++ Jalider-main/App/models.py
@@ -52,3 +52,34 @@ class CaboDeVela(models.Model):
 
     def __str__(self):
         return self.cabo_de_vela
+
+class Pneu(models.Model):
+    pneu = models.CharField(max_length=120)
+    km_pneu = models.IntegerField()
+
+    def __str__(self):
+        return self.pneu
+    
+class FiltroArCondicionado(models.Model):
+    filtro_ar_condicionado = models.CharField(max_length=120)
+    km_filtro_ar_condicionado = models.IntegerField()
+
+    def __str__(self):
+        return self.filtro_ar_condicionado
+
+class FiltroGas(models.Model):
+    filtro_gas = models.CharField(max_length=120)
+    km_filtro = models.IntegerField()
+
+    def __str__(self):
+        return self.filtro_gas
+    
+class Manutencao(models.Model):
+    carro_fk = models.CharField(max_length=1220)
+    OleoFiltroFK = models.ForeignKey(OleoFiltro, related_name='ManutencaoOleoFiltro', on_delete=models.CASCADE)
+    PastilhaDeFreioFK = models.ForeignKey(PastilhaDeFreio, related_name='ManutencaoPastilhaDeFreio', on_delete=models.CASCADE)
+    CorreiaDentalhaFK = models.ForeignKey(CorreiaDentalha, related_name='ManutencaoCorreiaDentalha', on_delete=models.CASCADE)
+    PneuFK = models.ForeignKey(Pneu, related_name='ManutencaoPneu', on_delete=models.CASCADE)
+    FiltroArCondicionadoFK = models.ForeignKey(FiltroArCondicionado, related_name='ManutencaoFiltroArCondicionado', on_delete=models.CASCADE)
+    FiltroGasFK = models.ForeignKey(FiltroGas, related_name='ManutencaoFiltroGas', on_delete=models.CASCADE)
+
